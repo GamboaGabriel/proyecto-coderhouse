d400c510a61f8a122dd73568dd638f48db5f4aa3
Gabriel Gamboa <maxisjrc@gmail.com>
2023-10-13 23:33:09 -0300

    Se agrega el nuevo formulario de formato desktop, y otros cambios de estilos q Q git commit

--- estilos/menu.js
+++ estilos/menu.js
@@ -9,3 +9,15 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
