a948e9b764446c5b437e06f647a3f6c09b671a40
Gabriel Gamboa <maxisjrc@gmail.com>
2023-11-01 23:18:44 -0300

    Arreglo del formulario mobile

--- javascript/menu.js
+++ javascript/menu.js
@@ -21,3 +21,18 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+const enlaceIniciarSesion = document.getElementById("enlace-iniciar-sesion");
+const enlaceRegistro = document.getElementById("enlace-registro");
+const registroFormulario = document.getElementById("registro-formulario");
+const inicioSesionFormulario = document.getElementById("inicio-sesion-formulario");
+
+enlaceIniciarSesion.addEventListener("click", () => {
+    registroFormulario.style.display = "none";
+    inicioSesionFormulario.style.display = "block";
+});
+
+enlaceRegistro.addEventListener("click", () => {
+    registroFormulario.style.display = "block";
+    inicioSesionFormulario.style.display = "none";
+});
